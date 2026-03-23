706c56501c18031bebc98e8bef686052a641fda1
Cameron <cameronlecocq@icloud.com>
2026-03-23 16:21:41 +0100

    Bundle three.js locally for 3D demo

--- ihm2/assets/robot-visual.js
+++ ihm2/assets/robot-visual.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.165.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.165.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from './vendor/three/build/three.module.js';
+import { OrbitControls } from './vendor/three/examples/jsm/controls/OrbitControls.js';
 
 export function createRobotVisual(container) {
   const scene = new THREE.Scene();
